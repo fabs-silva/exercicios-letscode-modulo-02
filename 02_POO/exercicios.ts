796781b040e86f02cc7f0185c722c60b59f1c66b
Fabiana Silva <fabianabz@yahoo.com>
2022-06-27 22:21:05 -0300

    exercícios de poo finalizados

--- 02_POO/exercicios.ts
+++ 02_POO/exercicios.ts
@@ -1,14 +1,14 @@
-//Exercício 01
+/* //Exercício 01
 
-/* const canetaVermelha = {
+const canetaVermelha = {
     marca: 'bic',
     tipo: 'esferográfica',
     cor: 'vermelha',
-} */
+} 
 
 //Exercício 02
 
-/* interface Televisao {
+interface Televisao {
     canal: number,
     volume: number,
     ligada: boolean,
@@ -51,11 +51,11 @@ const aumentaVolume = (tv: Televisao) => {
 
 const diminuiVolume = (tv: Televisao) => {
     return tv.volume -= 1;
-} */
+} 
 
 //Exercício 03
 
-/* class Pessoa {
+class Pessoa {
     constructor(public nomeCompleto: string,
         public email: string) {
 
@@ -75,11 +75,11 @@ const diminuiVolume = (tv: Televisao) => {
     get getPrimeiroNome(): string {
         return this.nomeCompleto.split(' ')[0];
     }
-} */
+} 
 
 //Exercício 04
 
-/* class Televisao {
+class Televisao {
     #canal: number;
     #volume: number;
     #ligada: boolean;
@@ -130,12 +130,11 @@ const diminuiVolume = (tv: Televisao) => {
     public diminuiVolume() {
         return this.#volume - 1;
     }
-} */
-
+} 
 
 //Exercício 05
 
-/* class Boletim {
+class Boletim {
     constructor(
         public nome: string,
         public ano: number,
@@ -182,11 +181,11 @@ const diminuiVolume = (tv: Televisao) => {
     get getMedias() {
         return this.medias;
     }
-} */
+} 
 
 //Exercício 06
 
-/* const categoriasCNH: string[] = ['A', 'B', 'C', 'D', 'E', 'AB', 'AC', 'AD', 'AE'];
+const categoriasCNH: string[] = ['A', 'B', 'C', 'D', 'E', 'AB', 'AC', 'AD', 'AE'];
 
 class CNH {
     constructor(
@@ -247,11 +246,11 @@ class CNH {
     get getCategoria() {
         return this.categoria;
     }
-} */
+} 
 
 //Exercício 07
 
-/* class Carro {
+class Carro {
     constructor(
         public marca: string,
         public cor: string,
@@ -346,119 +345,303 @@ class Time {
 
         throw new Error('Valor inválido. Digite W para vitória, L para derrota e D para empate.')
     }
-} */
+} 
 
 //Exercício 09
 
 class Veiculo {
-    public rodas: number;
+  public rodas: number;
 
-    constructor(
-        public usaCombustivel: boolean
-    ) {
+  constructor(public usaCombustivel: boolean) {
+    this.usaCombustivel = usaCombustivel;
+  }
 
-        this.usaCombustivel = usaCombustivel;
+  get getUsaCombustivel() {
+    return this.usaCombustivel;
+  }
+
+  get getRodas() {
+    return this.rodas;
+  }
+
+  set setRodas(rodas: number) {
+    if (this.rodas < 1) {
+      throw new Error("Um veículo deve ter pelo menos 1 roda");
     }
 
-    get getUsaCombustivel() {
-        return this.usaCombustivel;
-    }
-
-    get getRodas() {
-        return this.rodas;
-    }
-
-    set setRodas(rodas: number) {
-        if (this.rodas < 1) {
-            throw new Error("Um veículo deve ter pelo menos 1 roda");
-        }
-
-        this.rodas = rodas;
-    }
+    this.rodas = rodas;
+  }
 }
 
 class Carro extends Veiculo {
-    constructor(
-        public usaCombustivel: boolean,
-        public tipoDeCombustivel: string
-    ) {
-        super(usaCombustivel);
+  constructor(
+    public usaCombustivel: boolean,
+    public tipoDeCombustivel: string
+  ) {
+    super(usaCombustivel);
 
-        this.tipoDeCombustivel = 'álcool' || 'gasolina' || 'flex';
-    }
+    this.tipoDeCombustivel = "álcool" || "gasolina" || "flex";
+  }
 }
 
 class Bicicleta extends Veiculo {
-    constructor(
-        public usaCombustivel: boolean,
-        public infantil: boolean
-    ) {
-        super(usaCombustivel);
+  constructor(public usaCombustivel: boolean, public infantil: boolean) {
+    super(usaCombustivel);
 
-        if (this.rodas < 2) {
-            throw new Error("Uma bicicleta deve ter pelo menos 2 rodas");
-        }
-
-        this.infantil = infantil;
+    if (this.rodas < 2) {
+      throw new Error("Uma bicicleta deve ter pelo menos 2 rodas");
     }
+
+    this.infantil = infantil;
+  }
 }
 
-
 //Exercício 10
+class Programa {
+  constructor(public duracaoEmMinutos: number, public genero: string) {
+    this.duracaoEmMinutos = duracaoEmMinutos;
+    this.genero = genero;
+  }
 
+  get getDuracaoEmMinutos() {
+    return this.duracaoEmMinutos;
+  }
+
+  get getGenero() {
+    return this.genero;
+  }
+}
+
+class Seriado extends Programa {
+  constructor(
+    public duracaoEmMinutos: number,
+    public genero: string,
+    public anoDeInicio: number,
+    public numeroDeEpisodios: number,
+    public anoDeFim?: number
+  ) {
+    super(duracaoEmMinutos, genero);
+
+    this.anoDeInicio = anoDeInicio;
+    this.anoDeFim = anoDeFim;
+
+    if (this.numeroDeEpisodios < 1) {
+      throw new Error("O número de episódios deve ser maior do que 0");
+    }
+
+    this.numeroDeEpisodios = numeroDeEpisodios;
+  }
+
+  get getAnoDeInicio() {
+    return this.anoDeInicio;
+  }
+
+  get getAnoDeFim() {
+    return this.anoDeFim;
+  }
+
+  get getNumeroDeEpisodios() {
+    return this.numeroDeEpisodios;
+  }
+}
+
+class Filme extends Programa {
+  get getDuracaoEmHoras() {
+    const tempoHoras = Math.floor(this.duracaoEmMinutos / 60);
+    const tempoSobra = this.duracaoEmMinutos % 60;
+
+    return `${tempoHoras} h ${tempoSobra} min`;
+  }
+}
 //Exercício 11
 
 class Livro {
-    constructor(
-        public nome: string,
-        public autor: string,
-        public ano: number,
-    ) {
+  constructor(public nome: string, public autor: string, public ano: number) {
+    this.nome = nome;
+    this.autor = autor;
+    this.ano = ano;
+  }
 
-        this.nome = nome;
-        this.autor = autor;
-        this.ano = ano;
-    }
+  get getNome() {
+    return this.nome;
+  }
 
-    get getNome() {
-        return this.nome;
-    }
+  get getAutor() {
+    return this.autor;
+  }
 
-    get getAutor() {
-        return this.autor;
-    }
-
-    get getAno() {
-        return this.ano;
-    }
+  get getAno() {
+    return this.ano;
+  }
 }
 
 class Ebook extends Livro {
-    constructor(
-        public nome: string,
-        public autor: string,
-        public ano: number,
-        public numeroPaginas: number,
-        public numeroCapitulos: number
-    ) {
-        super(nome, autor, ano);
-        this.numeroPaginas = numeroPaginas;
-        this.numeroCapitulos = numeroCapitulos;
-    }
+  constructor(
+    public nome: string,
+    public autor: string,
+    public ano: number,
+    public numeroPaginas: number,
+    public numeroCapitulos: number
+  ) {
+    super(nome, autor, ano);
+    this.numeroPaginas = numeroPaginas;
+    this.numeroCapitulos = numeroCapitulos;
+  }
 }
 
 class AudioBook extends Livro {
-    constructor(
-        public nome: string,
-        public autor: string,
-        public ano: number,
-        public duracaoEmMinutos: number,
-    ) {
-        super(nome, autor, ano);
-        this.duracaoEmMinutos = duracaoEmMinutos;
-    }
+  constructor(
+    public nome: string,
+    public autor: string,
+    public ano: number,
+    public duracaoEmMinutos: number
+  ) {
+    super(nome, autor, ano);
+    this.duracaoEmMinutos = duracaoEmMinutos;
+  }
 }
 
 //Exercício 12
+class Dispositivo {
+  calcula() {
+    return "Calcula";
+  }
+}
+
+function MixinIlumina(superClass: typeof Dispositivo) {
+  return class extends superClass {
+    ilumina() {
+      return `Ilumina`;
+    }
+  };
+}
+
+function MixinAcessaInternet(superClass: typeof Dispositivo) {
+  return class extends superClass {
+    acessaInternet() {
+      return `Acessa a internet`;
+    }
+  };
+}
+
+function MixinTiraFotos(superClass: typeof Dispositivo) {
+  return class extends superClass {
+    tiraFotos() {
+      return `Tira fotos`;
+    }
+  };
+}
+
+function MixinRegistraPassos(superClass: typeof Dispositivo) {
+  return class extends superClass {
+    registraPassos() {
+      return `Registra Passos`;
+    }
+  };
+}
+
+class Relogio extends MixinAcessaInternet(MixinRegistraPassos(Dispositivo)) {}
+class Computador extends MixinAcessaInternet(MixinTiraFotos(Dispositivo)) {}
+class Celular extends MixinAcessaInternet(
+  MixinTiraFotos(MixinIlumina(MixinRegistraPassos(Dispositivo)))
+) {}
 
 //Exercício 13
+class Produto {
+  constructor(public nome: string, public valor: number) {
+    this.nome = nome;
+    this.valor = valor;
+  }
+
+  get getNome() {
+    return this.nome;
+  }
+
+  get getValor() {
+    return this.valor;
+  }
+}
+
+class Cartela {
+  constructor(public idDoEstabelecimento: string, public carimbos: number) {
+    this.idDoEstabelecimento = idDoEstabelecimento;
+    this.carimbos = carimbos;
+  }
+
+  get getIdDoEstabelecimento() {
+    return this.idDoEstabelecimento;
+  }
+
+  get getCarimbos() {
+    return this.carimbos;
+  }
+}
+
+class Prescricao {
+  constructor(public remedios: string[]) {
+    this.remedios = remedios;
+  }
+
+  get getRemedios() {
+    return this.remedios;
+  }
+}
+
+class Estabelecimento {
+  constructor(public id: number, public produtos: Produto[]) {
+    (this.id = id), (this.produtos = produtos);
+  }
+}
+
+function MixinRestaurante(superClass: typeof Estabelecimento) {
+  return class extends superClass {
+    constructor(
+      public id: number,
+      public produtos: Produto[],
+      public pontosParaResgatarPremio: number
+    ) {
+      super(id, produtos);
+      this.pontosParaResgatarPremio = pontosParaResgatarPremio;
+    }
+    resgatarPremio(cartela: Cartela) {
+      if (cartela.carimbos !== this.pontosParaResgatarPremio) {
+        throw new Error("Você ainda não atingiu a quantidade de carimbos");
+      }
+
+      cartela.carimbos = 0;
+      return "Prêmio";
+    }
+  };
+}
+
+function MixinFarmacia(superClass: typeof Estabelecimento) {
+  return class extends superClass {
+    comprarRemedios(prescricao: Prescricao, remedios: string[]) {
+      const remediosComprar = remedios.map((remedio) => {
+        return prescricao.remedios.filter((r) => remedio === r)[0];
+      });
+
+      if (!remediosComprar) {
+        throw new Error("Não há remédios a serem comprados");
+      }
+
+      return remediosComprar;
+    }
+  };
+}
+
+function MixinBar(superClass: typeof Estabelecimento) {
+  return class extends superClass {
+    pedirBebida(idade: number, produto: Produto) {
+      if (idade < 18) {
+        throw new Error("É proibido vender bebidas a menores de 18 anos");
+      }
+      return produto;
+    }
+  };
+}
+
+class Restaurante extends MixinRestaurante(Estabelecimento) {}
+class Bar extends MixinRestaurante(MixinBar(Estabelecimento)) {}
+class Farmacia extends MixinFarmacia(Estabelecimento) {}
+ */
