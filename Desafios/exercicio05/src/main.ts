88a5a8030ee906df6f7a1eb8a21ca1b170c4e978
f3152135 Fabiana de Cassia Silva Aranha <fabiana.aranha@bb.com.br>
2022-07-05 17:03:30 -0300

    desafio 05

--- Desafios/exercicio05/src/main.ts
+++ Desafios/exercicio05/src/main.ts
@@ -1,8 +1,34 @@
-import './style.css'
+import text from '../usernames.txt?raw';
 
-const app = document.querySelector<HTMLDivElement>('#app')!
+const textArray = text.toString().replace(/\r\n/g, '\n').split('\n');
 
-app.innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+const newTextArray = textArray.filter((str) => {
+  return str.length === 8 && str[2] === 'o' && str[5] === 'd';
+});
+console.log(newTextArray);
+
+const promiseArray: Promise<Response>[] = newTextArray.map((str) => {
+  return new Promise<Response>((resolve, reject) => {
+    const response: Promise<Response> = fetch(
+      `https://api.github.com/users/${str}`
+    ).then((response) => response.json());
+
+    response ? resolve(response) : reject('not found');
+  });
+});
+
+Promise.allSettled(promiseArray).then((results) => {
+  let notExistant: string[] = [];
+  let existant: string[] = [];
+  results.forEach((result) => {
+    if (result.value.name === null || result.value.name === undefined) {
+      notExistant = [...notExistant, result.value];
+    }
+    existant = [...existant, result.value.name];
+  });
+  console.log(`Usuários que não existem:
+  ${notExistant}
+  
+  Usuários que existem:
+  ${existant}`);
+});
